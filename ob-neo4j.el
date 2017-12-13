dff9a77492f4db7e543a9c6407b1bb1f38e0c666
Mark Borcherding <markborcherding@gmail.com>
2017-12-12 21:57:58 -0600

    inital commit

---
+++ ob-neo4j.el
@@ -0,0 +1,28 @@
+;;; Code:
+(require 'ob)
+(require 's)
+
+
+
+(defun org-babel-execute:neo4j (body params)
+  "Execute the provided cypher"
+
+  (let*
+      ((host     (or (cdr (assoc :host params)) "127.0.0.1"))
+      (username (cdr (assoc :username params)))
+      (password (cdr (assoc :password params)))
+      (cypher-params '((:host . host)
+                       (:password . password)
+                       (:username . username))))
+      
+    (message (prin1-to-string cypher-params))
+      (s-format "cypher ${:host}" 'aget cypher-params)
+  ))
+
+(defun org-babel-prep-session:neo4j (_session _params)
+  "Return an error if the :session header argument is set.
+   Neo4j does not support sessions."
+  (error "Neo4j sessions are nonsensical"))
+
+
+(provide 'ob-neo4j)
